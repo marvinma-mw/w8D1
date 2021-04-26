72b0fa32b7d79e57750feeb95e0b05f24e45198f
marvin <marvinma.mw@gmail.com>
2021-04-26 18:05:49 -0400

    question 01 02

--- project/data-structures-binary-tree-starter/problems/01-tree-node.js
+++ project/data-structures-binary-tree-starter/problems/01-tree-node.js
@@ -3,6 +3,12 @@
 // and constructs a valid binary tree node
 class TreeNode {
     // Your code here
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
 }
 
 module.exports = {
--- project/data-structures-binary-tree-starter/problems/02-tree-order.js
+++ project/data-structures-binary-tree-starter/problems/02-tree-order.js
@@ -1,15 +1,21 @@
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from a pre-order
 // traversal.
+// self-left-right
 function preOrderArray(root) {
-    // Your code here
+
+    if(!root) return [];
+    return [root.val,...preOrderArray(root.left),...preOrderArray(root.right)]
 }
 
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from an in-order
 // traversal.
+//left - self - right
 function inOrderArray(root) {
-    // Your code here
+
+    if(!root) return [];
+    return [...inOrderArray(root.left),root.val,...inOrderArray(root.right)]
 }
 
 // Implement a function that takes the root of a Binary Tree
@@ -17,8 +23,9 @@ function inOrderArray(root) {
 // traversal.
 function postOrderArray(root) {
     // Your code here
+    if(!root) return [];
+    return [...postOrderArray(root.left),...postOrderArray(root.right),root.val]
 }
-
 
 module.exports = {
   preOrderArray,
