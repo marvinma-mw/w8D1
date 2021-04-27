e932d74cea824441d583135d896638fa34be8b56
Vivian C <baongocche@gmail.com>
2021-04-27 09:20:23 -0700

    finished insert(val)

--- soloTrying/vivian/data-structures-binary-search-tree-starter/problems/01-bst.js
+++ soloTrying/vivian/data-structures-binary-search-tree-starter/problems/01-bst.js
@@ -9,12 +9,54 @@ class TreeNode {
 class BST {
     // Initialize an empty binary search tree
     constructor() {
-        // Your code here
+        this.root = null
     }
 
     // Insert a node into the binary search tree
     insert(val, currentNode=this.root) {
-        // Your code here
+        if (!currentNode) {
+            currentNode = new TreeNode(val)
+            return;
+        }
+
+        if (val < currentNode.val) {
+            if (!currentNode.left) {
+                currentNode.left = new TreeNode(val)
+                return;
+            } else {
+                this.insert(val, currentNode.left)
+            }
+
+        } else {
+            if(!currentNode.right) {
+                currentNode.right = new TreeNode(val)
+                return;
+            } else {
+                this.insert(val, currentNode.right)
+            }
+        }
+    }
+
+    insertIter(val, currentNode = this.root) {
+        // if (!this.root) {
+        //     this.root = new TreeNode(val)
+        // };
+
+        // while(currentNode) {
+        //     if (val < currentNode.val) {
+        //         if (!currentNode.left) {
+        //             currentNode.left = new TreeNode(val)
+        //         }else {
+        //             currentNode = currentNode.left
+        //         }
+        //     } else {
+        //         if (!currentNode.right) {
+        //             currentNode.right = new TreeNode(val)
+        //         } else {
+        //             currentNode = currentNode.right
+        //         }
+        //     }
+        // }
     }
 
     // Perform a recursive search through the binary search tree
